2f336e2024d11d81eff92605de85eea8a17790ef
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2018-11-22 10:24:43 -0500

    add models

---
+++ models.py
@@ -0,0 +1,25 @@
+from collections import namedtuple
+from enum import Enum, auto
+
+class Rank(Enum):
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 14
+
+class Suit(Enum):
+    SPADE = auto()
+    HEART = auto()
+    DIAMOND = auto()
+    CLUB = auto()
+
+Card = namedtuple('Card', ['rank', 'suit'])
