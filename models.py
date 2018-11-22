b53523da973312e01ee0d72b69846418604e1ec8
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2018-11-22 13:08:18 -0500

    add hand ordering

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from enum import IntEnum, Enum, auto
 from functools import total_ordering
+from operator import ne, lt
 
 class Rank(IntEnum):
     TWO = 2
@@ -32,3 +33,31 @@ class Card(namedtuple('Card', ['rank', 'suit'])):
 
     def __lt__(self, other):
         return self.rank < other.rank
+
+
+
+class HandCategory(IntEnum):
+    HIGH_CARD = 1
+    ONE_PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULL_HOUSE = 7
+    FOUR_OF_A_KIND = 8
+    STRAIGHT_FLUSH = 9
+
+
+@total_ordering
+class Hand(namedtuple('Hard', ['category', 'kickers'])):
+    def __eq__(self, other):
+        return (
+            self.category == other.category or
+            all(map(lambda x: ne(*x), zip(self.kickers, other.kickers)))
+        )
+
+    def __lt__(self, other):
+        return (
+            self.category < other.category or
+            all(map(lambda x: lt(*x), zip(self.kickers, other.kickers)))
+        )
--- test_models.py
+++ test_models.py
@@ -2,7 +2,7 @@ from operator import attrgetter
 
 from funcy import lmap
 
-from models import Rank, Suit, Card
+from models import Rank, Suit, Card, Hand, HandCategory
 
 
 def test_rank_order():
@@ -24,3 +24,19 @@ def test_card_sort_same_suit():
         Card(Rank.KING, Suit.SPADE),
     ]
     assert lmap(attrgetter('rank'), sorted(cards, reverse=True)) == [Rank.ACE, Rank.KING, Rank.TWO]
+
+
+def test_hand_order():
+    card1 = Card(Rank.TWO, Suit.SPADE)
+    card2 = Card(Rank.THREE, Suit.SPADE)
+    card3 = Card(Rank.TEN, Suit.SPADE)
+    card4 = Card(Rank.KING, Suit.SPADE)
+    card5 = Card(Rank.ACE, Suit.SPADE)
+    card6 = Card(Rank.ACE, Suit.HEART)
+
+    hand1 = Hand(HandCategory.HIGH_CARD, [card1, card2, card3, card4, card5])
+    hand2 = Hand(HandCategory.HIGH_CARD, [card1, card2, card3, card4, card5])
+    hand3 = Hand(HandCategory.ONE_PAIR, [card1, card2, card3, card5, card6])
+
+    assert hand1 == hand2
+    assert hand1 < hand3
